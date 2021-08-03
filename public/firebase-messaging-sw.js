53c9b31643357dc005d98b300f7535267e4f3a9b
Francis James <francizjamez@gmail.com>
2021-08-03 18:31:48 +0800

    enabled notification

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,10 +1,22 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("../firebase-messaging-sw.js")
-    .then(function (registration) {
-      console.log("Registration successful, scope is:", registration.scope);
-    })
-    .catch(function (err) {
-      console.log("Service worker registration failed, error:", err);
-    });
-}
+importScripts("https://www.gstatic.com/firebasejs/8.4.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.4.1/firebase-messaging.js");
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDTVXQ-aTWHTSEw_LVd1eRehl8nkVUALZo",
+  authDomain: "push-notif-sampl.firebaseapp.com",
+  projectId: "push-notif-sampl",
+  storageBucket: "push-notif-sampl.appspot.com",
+  messagingSenderId: "966221115923",
+  appId: "1:966221115923:web:bcaabe5f3e5260745305e6",
+  measurementId: "G-023PY6EH0S",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+messaging.requestPermission();
+
+messaging.onMessage(function (payload) {
+  console.log(payload);
+});
