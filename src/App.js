9360e7f609e74c72975f3a83bea4a6918e9bf116
Francis James <francizjamez@gmail.com>
2021-08-03 17:40:37 +0800

    added notif request and got token

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { askForPermissionToReceiveNotifications } from "./firebaseConfig";
 
 function App() {
+  useEffect(() => {
+    askForPermissionToReceiveNotifications();
+  });
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/firebaseConfig.js
@@ -0,0 +1,25 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDTVXQ-aTWHTSEw_LVd1eRehl8nkVUALZo",
+  authDomain: "push-notif-sampl.firebaseapp.com",
+  projectId: "push-notif-sampl",
+  storageBucket: "push-notif-sampl.appspot.com",
+  messagingSenderId: "966221115923",
+  appId: "1:966221115923:web:bcaabe5f3e5260745305e6",
+  measurementId: "G-023PY6EH0S",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const askForPermissionToReceiveNotifications = async () => {
+  try {
+    const messaging = firebase.messaging();
+    const token = await messaging.getToken();
+    console.log("Your token is:", token);
+    return token;
+  } catch (error) {
+    console.error(error);
+  }
+};
